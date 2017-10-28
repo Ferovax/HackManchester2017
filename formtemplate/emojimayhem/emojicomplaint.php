4f0f53d9557216ee99bfedc6e510a44e201f5c15
Ryan Smith <rlsmtih96118@gmail.com>
2017-10-28 23:28:24 +0100

    emojimayhem form
    not too sure if complete but its a start

---
+++ formtemplate/emojimayhem/emojicomplaint.php
@@ -0,0 +1,12 @@
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+<link href="css/stylish-portfolio.css" rel="stylesheet">
+
+<div class="col-sm-10 col-sm-offset-2">
+    <form action="../welcome.php" method="post">
+        Name: <input type="text"><br>
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+
+        <input type="submit">
+    </form>
