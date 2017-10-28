aa96e21ffdc010c4cd7af6eac5bfa34534e426af
Kyle Watson <kylewatson98@gmail.com>
2017-10-28 18:52:45 +0100

    linked to api

--- js/giphy.js
+++ js/giphy.js
@@ -7,10 +7,20 @@ function load(){
 
     $("#searchBox").keyup(function(){
         var q = $("#searchBox").val();
-        if(q.indexOf(" ") != -1){
-            var fakeJson = $.getJSON("stuff.json", dataLoad);
-            $("#searchBox").val("");
-            $("#imageSelection").html("");
+        if(q.indexOf(" ") == q.length-1){
+            if(q.length == 1){
+                $("#searchBox").val("");
+            }else {
+                //var fakeJson = $.getJSON("js/stuff.json", dataLoad);
+                var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+q+"&api_key="+gifkey+"&limit=5");
+                xhr.done(dataLoad);
+            }
+        }else if(q.indexOf(" ") > 0){
+            submitImage($("img").attr("src"), $("#searchBox").val());
+            $("#searchBox").val(q.substr(q.length-1,1));
+            if($("#searchBox").val() == " "){
+                $("#searchBox").val("");
+            }
         }
    }) ;
 }
@@ -26,7 +36,8 @@ function dataLoad(data) {
     });
 }
 
-function submitImage(uri){
+function submitImage(uri, text){
     $("<img>").attr("src", uri).appendTo("#imageSubmission");
+    $("<p>").text($("#searchBox").val()).appendTo("#imageSubmission");
     $("#imageSelection").html("");
 }
