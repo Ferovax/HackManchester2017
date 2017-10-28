37fc771f39fe54d4f14fc65a0e53136a2e9b8860
Ferovax <ferovax@gmail.com>
2017-10-28 22:26:48 +0100

    addingmorsecode

--- form.php
+++ form.php
@@ -56,14 +56,7 @@
         </div>
         <div class="form-group">
             <div class="col-sm-10 col-sm-offset-2">
-                <form action="welcome.php" method="post">
-                    Name: <input type="text" name="name"><br>
-                    E-mail: <input type="text" name="email"><br>
-                    Ready To Complain:<br>
-                    <input type="radio" name="complain" value="yes" checked> Yes<br>
-                    <input type="radio" name="complain" value="yes"> No<br>
 
-                    <input type="submit">
                 </form>
             </div>
         </div>
