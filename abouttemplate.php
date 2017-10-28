35b5e50fa96534447d7769593cb2382f6990b0cc
Ferovax <ferovax@gmail.com>
2017-10-28 15:38:41 +0100

    Website template 0.1
    First commit of website templace MVC Model

---
+++ abouttemplate.php
@@ -0,0 +1,5 @@
+<?php
+
+$view = new stdClass();
+$view->pageTitle = 'MVC Template';
+require_once('Views/abouttemplate.phtml');
---
+++ page1.php
@@ -0,0 +1,5 @@
+<?php
+
+$view = new stdClass();
+$view->pageTitle = 'Make a complaint';
+require_once('Views/page1.phtml');
