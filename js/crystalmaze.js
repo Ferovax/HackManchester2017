5e20185bfd6c6d00ecfe812f0ff38680149b529e
Ferovax <ferovax@gmail.com>
2017-10-29 01:48:59 +0000

    putinscrystalmazev1

---
+++ js/crystalmaze.js
@@ -0,0 +1,18 @@
+function alertBox() {
+    alert("You can not defeat mighty putin");
+}
+function alertBox2() {
+    alert("Red Summit is too strong for you");
+}
+function alertBox3() {
+    alert("The fury is strong in this one");
+}
+function alertBox4() {
+    alert("Are you even trying?");
+}
+function alertBox5() {
+    alert("You can not defeat mighty putin");
+}
+function alertBox6() {
+    alert("You can not defeat mighty putin");
+}
--- putinscrystalmaze.php
+++ putinscrystalmaze.php
@@ -1,7 +1,60 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Vurie
- * Date: 28/10/2017
- * Time: 21:45
- */
+<html>
+<head>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+    <link href="css/crystalmaze.css" rel="stylesheet">
+    <script src="js/giphy.js"></script>
+    <script src="jquery-3.2.1.min.js"></script>
+    <script src="js/countdownclock.js"></script>
+    <script src="js/crystalmaze.js"></script>
+</head>
+<body onload="load()">
+<h1>Welcome to Putin's Crystal Maze.</h1>
+<h2>You have x time to complete this form</h2>
+<h2>Nuclear launch in:<div id="demo"></div></h2>
+<form class="form-horizontal" role="form" method="post" action="js/form-action.php">
+    <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Name</label>
+        <div class="col-sm-10">
+            <input type="text" onclick="alertBox()" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="email" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+            <input type="email" onclick="alertBox2()" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="message" class="col-sm-2 control-label">Message</label>
+        <div class="col-sm-10">
+            <textarea onclick="alertBox3()" class="form-control" rows="4" name="message"></textarea>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+        <div class="col-sm-10">
+            <input type="text" onclick="alertBox4()" class="form-control" id="human" name="human" placeholder="Your Answer">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-10 col-sm-offset-2">
+            <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-10 col-sm-offset-2">
+            <! Will be used to display an alert to the user>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-10 col-sm-offset-2">
+                <input id="urls" name="urls" type="text" hidden>
+</form>
+</div>
+</div>
+</div>
+</form>
+</body>
+</html>
