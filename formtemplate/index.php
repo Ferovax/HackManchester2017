98304d53ed4d2fe937b513ca6a9bc139cb815f9b
Ryan Smith <rlsmtih96118@gmail.com>
2017-10-28 17:07:07 -0700

    Email form update
    moved email to a seperate page for emojis

--- formtemplate/index.php
+++ formtemplate/index.php
@@ -1,6 +1,5 @@
 <form action="welcome.php" method="post">
     Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
     Ready To Complain:<br>
     <input type="radio" name="complain" value="yes" checked> Yes<br>
     <input type="radio" name="complain" value="yes"> No<br>
--- formtemplate/welcome.php
+++ formtemplate/welcome.php
@@ -1,2 +1,3 @@
 Hi <?php echo htmlspecialchars($_POST['name']); ?>.
 <?php echo $_POST['complain'];?>. You are ready to complain.
+<?php echo $_POST['email'];?>
